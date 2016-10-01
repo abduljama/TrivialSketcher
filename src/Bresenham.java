95de686fd99b1143ca63483c4234d1aeaa2c62a7
abduljama <abduljama2009@hotmail.com>
2016-10-01 12:45:29 +0300

    A small bug fix

--- src/Bresenham.java
+++ src/Bresenham.java
@@ -16,7 +16,7 @@ public class Bresenham extends Applet {
     int xn , yn ;
 
     // Delta X & Y
-    double dx,dy;
+    double dx,dy, dydx1,dydx2;
 
     double  pX;
 
@@ -30,13 +30,18 @@ public class Bresenham extends Applet {
         this.yn=yn;
     }
 
+    public Bresenham () {}
+
 
     public void paint(Graphics g) {
-     //   x0= -8 ; y0= -4; xn= 0;yn=0 ;
+       x0= -1 ; y0= -1; xn= -6;yn= 1  ;
         g.fillOval(x0, y0, 5, 5);
         g.fillOval(xn, yn, 5, 5);
         dx = xn - x0;
         dy = yn - y0;
+        dydx1=dy-dx;
+        dydx2=dy+dx;
+
 
         //  Computes total number of  points  required to  plot the line
         if  ( Math.abs(xn) > Math.abs(x0))
@@ -47,7 +52,7 @@ public class Bresenham extends Applet {
         double  grad = dy/dx;
         if  ( grad > 0 ) {
             System.out.println("Gradient is positive");
-            pX= 2*dy-dx;
+            pX= 2*dydx1;
             System.out.println( "pX : " + pX + " X : " + x0 +" Y : " +y0 );
             for (int k = 1; k < totalPoints; k++) {
                 if (pX < 0) {
@@ -56,27 +61,29 @@ public class Bresenham extends Applet {
                     System.out.println( "pX : " + pX + " X : " + x0 +" Y : " +y0 );
                 } else {
                     g.fillOval(x0++, y0++, 5, 5);
-                    pX = pX + (2 * (dy - dx));
+                    pX = pX + (2 * dydx1);
                     System.out.println( "pX : " + pX + " X : " + x0 +" Y : " +y0 );
                 }
 
             }
         }
         else {
-            //  Cannot
+
             System.out.println("Gradient is negative");
-            pX= -2*dy-dx;
+            pX= (-2)*dydx1;
             System.out.println( "pX : " + pX + " X : " + x0 +" Y : " +y0 );
             for (  int k = 1; k < totalPoints; k++){
                 if (pX <0){
+                    x0++;
                     //plot pixel (x0++  & y0 )
                     // unable to plot -ve values  of  X and Y
                     pX= pX- (2 * dy);
                     System.out.println( "pX : " + pX + " X : " + x0 +" Y : " +y0 );
                 }else {
+                    x0++ ; y0++;
                     //plot pixel (x0++  & y0++ )
                     // unable to plot -ve values  of  X and Y
-                    pX=pX - (2 * (dy + dx));
+                    pX=pX - (2 * dydx2);
                     System.out.println( "pX : " + pX + " X : " + x0 +" Y : " +y0 );
                 }
             }
@@ -84,19 +91,19 @@ public class Bresenham extends Applet {
         }
     }
     public  void main(String[] args) {
-
-        Scanner reader = new Scanner(System.in);  // Reading from System.in
-        System.out.println("Enter  intial  X-cordinate: ");
-        int int_x = reader.nextInt();
-        System.out.println("Enter  intial  Y-cordinate: ");
-        int  int_y = reader.nextInt();
-        System.out.println("Enter  final  X-cordinate: ");
-        int last_x = reader.nextInt();
-        System.out.println("Enter  final  Y-cordinate: ");
-        int last_y= reader.nextInt();
+//
+//        Scanner reader = new Scanner(System.in);  // Reading from System.in
+//        System.out.println("Enter  intial  X-cordinate: ");
+//        int int_x = reader.nextInt();
+//        System.out.println("Enter  intial  Y-cordinate: ");
+//        int  int_y = reader.nextInt();
+//        System.out.println("Enter  final  X-cordinate: ");
+//        int last_x = reader.nextInt();
+//        System.out.println("Enter  final  Y-cordinate: ");
+//        int last_y= reader.nextInt();
 
         Graphics g = getGraphics();
-        Bresenham bresenham = new Bresenham( int_x,int_y,last_x ,last_y);
+        Bresenham bresenham = new Bresenham();
         bresenham.paint(g);
 
 
